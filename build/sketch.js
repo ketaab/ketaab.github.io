147532e969ed0306ba964d4411624b482bd188b0
Salah Salah <SalahSalah@outlook.com>
2021-02-08 13:58:07 -0700

    Update sketch.js

--- build/sketch.js
+++ build/sketch.js
@@ -6,7 +6,7 @@ let img;
 
 function preload() {
   classifier = ml5.imageClassifier('MobileNet');
-  img = loadImage('images/212740-Colorful-Bird.jpg');
+  img = loadImage('images/Yellow-chrysanthemums-Flowers-wallpapers-pictures-photos-Download-1024x768.jpg');
 }
 
 function setup() {
