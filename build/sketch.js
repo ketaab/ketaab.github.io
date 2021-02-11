d48e7be98100d46f9ae8486aa7025bc8b7192de6
Ashley   Roberts <42703511+awefiown@users.noreply.github.com>
2021-02-10 19:47:01 -0500

    Update sketch.js

--- build/sketch.js
+++ build/sketch.js
@@ -6,7 +6,7 @@ let img;
 
 function preload() {
   classifier = ml5.imageClassifier('MobileNet');
-  img = loadImage('images/bird1.png');
+  img = loadImage('images/t1.jpg');
 }
 
 function setup() {
